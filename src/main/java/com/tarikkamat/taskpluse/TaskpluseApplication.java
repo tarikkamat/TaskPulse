f6825e9fd00f4eb864badebde435e9788292efc4
Tarık KAMAT <tarikkamat2@gmail.com>
2025-03-12 07:25:22 +0300

    first commit

---
+++ src/main/java/com/tarikkamat/taskpluse/TaskpluseApplication.java
@@ -0,0 +1,13 @@
+package com.tarikkamat.taskpluse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskpluseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskpluseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarikkamat/taskpluse/TaskpluseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarikkamat.taskpluse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskpluseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
