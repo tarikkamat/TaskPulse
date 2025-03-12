bb8134f54484646998d39e31e304a77442a0d11a
Tarık KAMAT <tarikkamat2@gmail.com>
2025-03-12 08:07:50 +0300

    Add test profile configuration and H2 database settings for testing

--- src/test/java/com/tarikkamat/taskpluse/TaskpluseApplicationTests.java
+++ src/test/java/com/tarikkamat/taskpluse/TaskpluseApplicationTests.java
@@ -2,8 +2,10 @@ package com.tarikkamat.taskpluse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskpluseApplicationTests {
 
 	@Test
